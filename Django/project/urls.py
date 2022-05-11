f13f9563b6e587c4fa71d64fd6fb8d7266d9ad58
tyc0919 <10846010@ntub.edu.tw>
2022-05-11 17:42:08 +0800

    Views Written
    Wrote views for every template for now

--- Django/project/urls.py
+++ Django/project/urls.py
@@ -1,7 +1,40 @@
 from django.urls import path, include
 from django.urls import re_path as url
-from  .views import test
+from  . import views
 
 urlpatterns = [
-    path('test/', test, name="test")
-]
+    #path('test/', test, name="test"),
+    path('', views.index, name="index"),
+    path('signin/', views.signin, name="signin"),
+    path('signup/', views.signup, name="signup"),
+    path('social/', views.social, name="social"),
+    path('forgetpasswd/', views.forgetpasswd, name="forgetpasswd"),
+    path('logout/', views.logout, name="logout"),
+    #---------------------------------------------
+    path('event_index/', views.event_index, name="event_index"),
+    #---------------------------------------------
+    path('information/', views.information, name="information"),
+    #---------------------------------------------
+    path('my_proposal/', views.my_proposal, name="my_proposal"),
+    path('proposal_checked/', views.proposal_checked, name="proposal_checked"),
+    path('proposal_create/', views.proposal_create, name="proposal_create"),
+    path('proposal_create_function1/', views.proposal_create_function1, name="proposal_create_function1"),
+    path('proposal_create_function2/', views.proposal_create_function2, name="proposal_create_function2"),
+    path('proposal_create_function2_1/', views.proposal_create_function2_1, name="proposal_create_function2_1"),
+    path('proposal_create_function3/', views.proposal_create_function3, name="proposal_create_function3"),
+    path('proposal_create_function4/', views.proposal_create_function4, name="proposal_create_function4"),
+    path('proposal_post/', views.proposal_post, name="proposal_post"),
+    #---------------------------------------------
+    path('mywork/', views.mywork, name="mywork"),
+    path('work_checked_proposal/', views.work_checked_proposal, name="work_checked_proposal"),
+    path('work_checked/', views.work_checked, name="work_checked"),
+    path('work_checked2/', views.work_checked2, name="work_checked2"),
+    path('work_checked3/', views.work_checked3, name="work_checked3"),
+    path('work_checked4/', views.work_checked4, name="work_checked4"),
+    #---------------------------------------------
+    path('colla_index/', views.colla_index, name="colla_index"),
+    path('colla_checked/', views.colla_checked, name="colla_checked"),
+    #---------------------------------------------
+    path('my_community/', views.my_community, name="my_community"),
+    path('blog_details/', views.blog_details, name="blog_details"),
+]   
--- Django/project/views.py
+++ Django/project/views.py
@@ -1,3 +1,101 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 # Create your views here.
+
+
+#----------Not Required Login----------
+
+def index(request):
+    return render(request, 'index.html')
+
+def signin(request):
+    return render(request, 'signin.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def social(request):
+    return render(request, 'social.html')
+
+def forgetpasswd(request):
+    return render(request, 'forgetpasswd.html')
+
+def logout(request):
+    return render(request, 'index.html')
+
+#----------Required Login----------
+
+def event_index(request):
+    return render(request, 'event_index.html')
+
+##----------User Area-----------
+
+def information(request):
+    return render(request, 'information.html')
+
+##----------Proposal----------
+
+def my_proposal(request):
+    return render(request, 'my_proposal.html')
+
+def proposal_checked(request):
+    return render(request, 'proposal_checked.html')
+
+def proposal_create(request):
+    return render(request, 'proposal_create.html')
+
+def proposal_create_function1(request):
+    return render(request, 'proposal_create_function1.html')
+
+def proposal_create_function2(request):
+    return render(request, 'proposal_create_function2.html')
+
+def proposal_create_function2_1(request):
+    return render(request, 'proposal_create_function2_1.html')
+
+def proposal_create_function3(request):
+    return render(request, 'proposal_create_function3.html')
+
+def proposal_create_function4(request):
+    return render(request, 'proposal_create_function4.html')
+
+def proposal_post(request):
+    return render(request, 'proposal_post.html')
+
+##----------Work----------
+
+def mywork(request):
+    return render(request, 'mywork.html')
+
+def work_checked_proposal(request):
+    return render(request, 'work_checked_proposal.html')
+
+def work_checked(request):
+    return render(request, 'work_checked.html')
+
+def work_checked2(request):
+    return render(request, 'work_checked2.html')
+
+def work_checked3(request):
+    return render(request, 'work_checked3.html')
+
+def work_checked4(request):
+    return render(request, 'work_checked4.html')
+
+##----------Collab----------
+
+def colla_index(request):
+    return render(request, 'colla_index.html')
+
+def colla_checked(request):
+    return render(request, 'colla_checked.html')
+
+##----------Social----------
+
+def my_community(request):
+    return render(request, 'my_community.html')
+
+def blog_details(request):
+    return render(request, 'blog_details.html')
+
+#----------custom functions-----------
