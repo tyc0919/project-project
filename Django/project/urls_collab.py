92b2da2bb8570cb86ce7ba2b765da633b465306d
BeDaShao <10846015@ntub.edu.tw>
2022-06-07 14:39:30 +0800

    工作頁面連結資料
    // models
    - 應該要用tinyint的欄位，在這邊還是用int，但是不影響view
    >> 合起來再統一處理
    - 修改  拿掉order欄位的unique限制 (job, job_detail)
    - 新增 shop.serve_city : 存放店家的服務地點, 暫時取代多對多的serve_city
    
    // templates
    - 新增 work_content_update.html
    - 新增 work_detail_update.html
    - 新增 work_detail_remove.html
    - 拔除 work_checked.html
    >> 將入口都改成work_checked3.html
    
    // others
    - proposal_work_checked 參數只用 job_id
    
    // need to fix
    - work_detail_remove 要顯示工作細項的數字(work_checked3的counter)

--- Django/project/urls_collab.py
+++ Django/project/urls_collab.py
@@ -4,5 +4,5 @@ from  . import views_collab
 
 urlpatterns = [
     path('colla_index/', views_collab.colla_index, name="colla_index"),
-    path('colla_checked/', views_collab.colla_checked, name="colla_checked"),
+    path('colla_checked/<str:shop_email>', views_collab.colla_checked, name="colla_checked"),
 ]   
--- Django/project/urls_work.py
+++ Django/project/urls_work.py
@@ -4,9 +4,12 @@ from  . import views_work
 
 urlpatterns = [
     path('mywork/', views_work.mywork, name="mywork"),
-    path('work_checked_proposal/', views_work.work_checked_proposal, name="work_checked_proposal"),
-    path('work_checked/', views_work.work_checked, name="work_checked"),
-    path('work_checked2/', views_work.work_checked2, name="work_checked2"),
-    path('work_checked3/', views_work.work_checked3, name="work_checked3"),
-    path('work_checked4/', views_work.work_checked4, name="work_checked4"),
+    path('proposal_work_checked/<int:job_id>', views_work.proposal_work_checked, name="proposal_work_checked"),
+    path('work_checked/<int:job_id>', views_work.work_checked, name="work_checked"),
+    path('work_checked2/<int:job_id>', views_work.work_checked2, name="work_checked2"),
+    path('work_checked3/<int:job_id>', views_work.work_checked3, name="work_checked3"),
+    path('work_checked4/<int:job_id>', views_work.work_checked4, name="work_checked4"),
+    path('work_detail_remove/<int:job_detail_id>', views_work.work_detail_remove, name="work_detail_remove"),
+    path('work_detail_update/<int:job_detail_id>', views_work.work_detail_update, name="work_detail_update"),
+    path('work_content_update/<int:job_id>', views_work.work_content_update, name="work_content_update"),
 ]   
