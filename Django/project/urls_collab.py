5b810e9d00e764ef33bb9b22aa2ee0c61f4d5bbc
tyc0919 <10846010@ntub.edu.tw>
2022-11-09 23:31:09 +0800

    Removed obsolete files

--- Django/project/urls_collab.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from django.urls import re_path as url
-from  . import views_collab
-
-urlpatterns = [
-    path('colla_index/', views_collab.colla_index, name="colla_index"),
-    path('colla_checked/<str:shop_email>', views_collab.colla_checked, name="colla_checked"),
-]   
